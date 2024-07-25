cee4ee1764c3504f1ab3b604ae79a0b11f186835
Ocean Fuaga <oceansfuaga@gmail.com>
2024-07-24 23:09:53 -0500

    Set up function skeletons

--- script.js
+++ script.js
@@ -1,38 +1,83 @@
 let num1 = 0;
 let operator = '';
 let num2 = 0;
+let displayText = '';
 
-let displayStr = '0';
+let activeNum = 'num1';
 
 const display = document.querySelector('h1');
 const numBtns = document.querySelectorAll('.number');
 const opBtns = document.querySelectorAll('.operator');
+const equalBtn = document.querySelector('#equals');
 const clearBtn = document.querySelector('#clear');
 
-// Add listener to every num btn
 numBtns.forEach(btn => {
-    btn.addEventListener('click', () => populateDisplay(btn.textContent))
-})
+    btn.addEventListener('click', (e) => {
+        updateDisplay(e.target.innerText);      
+        setNum(e.target.innerText);
+    });
+});
 
-// Add listener to every op btn
 opBtns.forEach(btn => {
-    btn.addEventListener('click', () => console.log(btn.textContent))
-})
+    btn.addEventListener('click', (e) => {
+        setOperator(e.target.innerText);
+    });
+});
+
+equalBtn.addEventListener('click', () => {
+    operate(operator, num1, num2);
+});
 
 clearBtn.addEventListener('click', () => {
-    clear();
-})
+   
+});
 
-function operate(num1, operator, num2) {
+function add(num1, num2) {
+    return num1 + num2;
+};
+
+function subtract(num1, num2) {
+    return num1 - num2;
+};
+
+function multiply(num1, num2) {
+    return num1 * num2;
+};
+
+function divide(num1, num2) {
+    return num1 / num2;
+};
+
+function operate(operator, num1, num2) {
+    num1 = Number(num1);
+    num2 = Number(num2);
+    let result;
+    switch (operator) {
+        case '+':
+            result = add(num1, num2);
+            break;
+        case '-':
+            result = subtract(num1, num2);
+            break;
+        case '*':
+            result = multiply(num1, num2);
+            break;
+        case '/':
+            result = divide(num1, num2);
+            break;
+    };
+    display.innerText = result;
+};
+
+function setNum(num) {
     
 };
 
-function populateDisplay(text) {
-    displayStr === '0'? displayStr = text : displayStr += text;
-    display.textContent = displayStr;
+function setOperator(symbol) {
+    operator = symbol;
 };
 
-function clear() {
-    displayStr = '0';
-    display.textContent = displayStr;
-}
+function updateDisplay(num) {
+    displayText += num;
+    display.innerText = displayText.substring(0, 8);
+};
